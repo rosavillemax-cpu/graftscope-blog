7333c625307b8a408a457b1fdd0c27f514a041ff
rosavillemax-cpu <rosavillemax@gmail.com>
2026-05-19 12:52:50 +0300

    Add redirects to clinixglow.com/blog

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: '/blog',
   assetPrefix: '/blog',
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://www.clinixglow.com/blog/:path*',
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
